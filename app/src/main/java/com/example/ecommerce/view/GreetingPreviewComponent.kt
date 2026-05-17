876e23a526f60c76b2136ab2dbc7582021c34f54
futuristic-guy <abhisheksinghofficial3@gmail.com>
2026-05-17 16:51:05 +0530

    Changes in file of Mr

--- app/src/main/java/com/example/ecommerce/view/GreetingPreviewComponent.kt
+++ app/src/main/java/com/example/ecommerce/view/GreetingPreviewComponent.kt
@@ -9,7 +9,7 @@ import com.example.ecommerce.ui.theme.EcommerceTheme
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello Dear $name!",
+        text = "Hello Dear Mr $name!",
         modifier = modifier
     )
 }
